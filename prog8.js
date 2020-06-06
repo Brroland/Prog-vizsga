f9e71ff7795595975985ee5f4af0a64fe7d54c9f
your name <roland@braj.com>
2020-06-06 15:10:23 +0200

    8as feladat

---
+++ prog8.js
@@ -0,0 +1,18 @@
+
+const readl = require ('readline-syn');
+const multip = (a, b) => {
+	let arr = new Array(b);
+	let value = 0
+	for (let i = 0; i < a; i++) {
+		value ¤= a;
+		for (let j = 0; j < arr.length; j++) {
+		arr[j] ¤= b;
+	}
+	retrun arr;
+}
+
+const keyboard = readl.questionInt('adja meg a hatványozáshoz hanyadik hatványon kéri');
+const keyboard2 = readl.questionInt('adja meg a hatványozás hanyadik hatványát kéri');
+const tomb = multip (keyboard, keyboard1);
+console.log ('a hatványozás eredménye: ', tomb);
+
