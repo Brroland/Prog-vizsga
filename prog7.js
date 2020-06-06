7798a9b17889dc34b819046a0234e5f26e0b0aa3
your name <roland@braj.com>
2020-06-06 15:12:01 +0200

    7es feladat

---
+++ prog7.js
@@ -0,0 +1,28 @@
+const suma = (a, b) => {
+	let sum = a + b;
+	return sum;
+
+const kivonas = (a, b) => {
+	if (a > b) {
+		let out = a - b;
+		} else {
+		out = b - a;
+	return out;
+};
+
+const multip = (a, b) => {
+	let mult = a ¤ b;
+	return mult;
+};
+
+const dived = (a, b) => {
+	let dive = a / b,
+	return dive;
+};
+
+module.exports = {
+	sum,
+	kivonas,
+	multip,
+	dived
+	}
